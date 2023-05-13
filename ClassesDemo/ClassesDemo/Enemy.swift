8ebd68cb3cdaaeff55ecc91856be0cca9ce0e16a
zekigklr <77938037+zekigklr@users.noreply.github.com>
2023-05-13 19:17:45 +0300

    Classes and Inheritance
    Example

--- ClassesDemo/ClassesDemo/Enemy.swift
+++ ClassesDemo/ClassesDemo/Enemy.swift
@@ -1,4 +1,12 @@
 
 class Enemy {
+    var health = 100
+    var attackStrength = 10
     
+    func move() {
+        print("Walk forwards.")
+    }
+    func attack() {
+        print("Land a hit , does \(attackStrength) damage.")
+    }
 }
