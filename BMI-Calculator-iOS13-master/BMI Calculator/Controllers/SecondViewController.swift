ac6c200b091473d6efec9a2546b4224ffe170597
zekigklr <77938037+zekigklr@users.noreply.github.com>
2023-05-15 23:13:15 +0300

    Create
    How to create ui programmatically and pass data between viewcontrollers.

---
+++ BMI-Calculator-iOS13-master/BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Zeki Gökler on 13.05.2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+         super.viewDidLoad()
+    }
+}
