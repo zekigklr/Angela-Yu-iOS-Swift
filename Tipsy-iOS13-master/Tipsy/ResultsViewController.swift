3b883ff6975354623a79f759c2d09cd95ae7fc6f
zekigklr <77938037+zekigklr@users.noreply.github.com>
2023-05-17 13:54:44 +0300

    Advanced Swift Programming Challenge
    Step 2: Create IBActions and IBOutlets for the Calculator Screen

---
+++ Tipsy-iOS13-master/Tipsy/ResultsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Zeki Gökler on 17.05.2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
