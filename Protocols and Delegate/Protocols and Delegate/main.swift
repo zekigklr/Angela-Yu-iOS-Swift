e9d179847cc6b0752c9e35777f6d75e343977269
zekigklr <77938037+zekigklr@users.noreply.github.com>
2023-05-21 14:27:15 +0300

    147. An Example of Protocols and Delegates in Practice

--- Protocols and Delegate/Protocols and Delegate/main.swift
+++ Protocols and Delegate/Protocols and Delegate/main.swift
@@ -1,11 +1,56 @@
-//
-//  main.swift
-//  Protocols and Delegate
-//
-//  Created by Zeki Gökler on 20.05.2023.
-//
 
-import Foundation
+protocol AdvancedLifeSupport {
+    func performCPR()
+    
+}
 
-print("Hello, World!")
+class EmergencyCallHandler {
+    var delegate: AdvancedLifeSupport?
+    
+    func assessSituation() {
+        print("Can you tell me what happened?")
+    }
+    
+    func medicalEmergency() {
+        delegate?.performCPR()
+    }
+}
+
+struct Paramedic: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    func performCPR() {
+        print("The paramedic does chest compressions, 30 per seconds.")
+    }
+}
+
+class Doctor: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    func performCPR() {
+        print("The doctor does chest compressions, 30 per seconds.")
+
+    }
+    func useStethescope() {
+        print("Listening for heart sounds.")
+    }
+    
+}
+
+class Surgeon: Doctor {
+    override func performCPR() {
+        super.performCPR()
+        print("Sings staying alive by the BeeGees")
+    }
+    func useElectricDrill() {
+        print("Whirr....")
+    }
+}
+
+let emilio = EmergencyCallHandler()
+let angela = Surgeon(handler: emilio)
 
