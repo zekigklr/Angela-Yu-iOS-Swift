5b4f89cdcd1103f2190d0e756259e52d9f14ad27
zekigklr <77938037+zekigklr@users.noreply.github.com>
2023-05-23 22:06:41 +0300

    JSON Decoding
    151

---
+++ Clima-iOS13-master/Clima/Model/WeatherData.swift
@@ -0,0 +1,11 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Zeki Gökler on 23.05.2023.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct 
--- Clima-iOS13-master/Clima/Model/WeatherManager.swift
+++ Clima-iOS13-master/Clima/Model/WeatherManager.swift
@@ -23,22 +23,24 @@ struct WeatherManager {
             //2.Create a URLSession
             let session = URLSession(configuration: .default)
             //3.Give the session a task
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                    
+                }
+            }
             
             //4.Start the task
             task.resume()
         }
     }
-    
-    func handle(data: Data?, response:URLResponse?, error: Error?){
-        if error != nil {
-            print(error!)
-            return
-        }
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
-        }
+    func parseJSON() {
+        
     }
+    
     
 }
