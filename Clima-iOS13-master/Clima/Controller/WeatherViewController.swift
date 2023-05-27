04e77953a2dec05c0f68e0c49c8a3b0be37bb49a
zekigklr <77938037+zekigklr@users.noreply.github.com>
2023-05-27 22:50:52 +0300

    Networking, JSON Parsing, APIs and Core Location (FINISH)
    159. Using CoreLocation to get Location Data

--- Clima-iOS13-master/Clima/Controller/WeatherViewController.swift
+++ Clima-iOS13-master/Clima/Controller/WeatherViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class WeatherViewController: UIViewController {
     
@@ -18,14 +19,26 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var cityLabel: UILabel!
     
     var weatherManager = WeatherManager()
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        
         
         weatherManager.delegate = self
         searchTextField.delegate = self
         
     }
+    
+    @IBAction func locationPressed(_ sender: UIButton) {
+        locationManager.requestLocation()
+        
+    }
+    
 }
 
 //MARK: - UITextFieldDelegate
@@ -66,6 +79,7 @@ extension WeatherViewController: WeatherManagerDelegate {
         DispatchQueue.main.async {
             self.temperatureLabel.text = weather.temperatureString
             self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+            self.cityLabel.text = weather.cityName
         }
     }
     
@@ -73,5 +87,22 @@ extension WeatherViewController: WeatherManagerDelegate {
         print(error)
     }
     
+}
+
+//MARK: - CLLocationManagerDelegate
+
+extension WeatherViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locationManager.stopUpdatingLocation()
+        if let location = locations.last {
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
 }
 
