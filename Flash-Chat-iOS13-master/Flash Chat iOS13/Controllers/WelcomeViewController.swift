272085b89ef116480f16dfc316ce285ffb573ee2
zekigklr <77938037+zekigklr@users.noreply.github.com>
2023-06-03 20:40:07 +0300

    Firebase Cloud Firestone, TableViews and Cocoapod Dependencies
    182. How to use the CLTypingLabel Pod

--- Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
@@ -15,17 +16,20 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = ""
-        var charIndex = 0.0
-        let titleText = "⚡️FlashChat"
-        for letter in titleText {
-            print("-")
-            print(0.1 * charIndex)
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
+       // titleLabel.text = "⚡️FlashChat"
+        
+            titleLabel.text = ""
+            var charIndex = 0.0
+            let titleText = "⚡️FlashChat"
+            for letter in titleText {
+                print("-")
+                print(0.1 * charIndex)
+                Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                    self.titleLabel.text?.append(letter)
+                }
+                charIndex += 1
             }
-            charIndex += 1
-        }
+        
 
        
     }
