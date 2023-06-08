554bbdf087fcdc27e814e062f8da11d2ff03f5ba
zekigklr <77938037+zekigklr@users.noreply.github.com>
2023-06-08 13:05:12 +0300

    SwiftUI

---
+++ I-Am-Rich-iOS13-SwiftUI-Completed-master/I Am Rich/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by Angela Yu on 06/09/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
