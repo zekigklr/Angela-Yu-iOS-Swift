b67046f0fa520d2df62e951778b61cd7e9e1a548
zekigklr <77938037+zekigklr@users.noreply.github.com>
2023-06-09 13:07:27 +0300

    New project

---
+++ Flash Chat/Flash Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flash Chat
+//
+//  Created by Zeki Gökler on 9.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
