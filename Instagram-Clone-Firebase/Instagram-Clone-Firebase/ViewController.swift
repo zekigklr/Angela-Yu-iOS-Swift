cfbfb1c3cb4fde38e6816c8ba7056daea734ea78
zekigklr <77938037+zekigklr@users.noreply.github.com>
2023-06-15 15:10:11 +0300

    .

---
+++ Instagram-Clone-Firebase/Instagram-Clone-Firebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram-Clone-Firebase
+//
+//  Created by Zeki Gökler on 15.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
